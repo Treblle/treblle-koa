d177fdb65de01a5032776db7af867f8c777a29a2
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-20 19:14:37 +0100

    chore: setup example Koa project

--- example.js
+++ example.js
@@ -1,7 +1,20 @@
+require('dotenv').config()
+
 const Koa = require('koa')
+const treblle = require('./index')
 const app = new Koa()
+
+// x-response-time
+app.use(
+  treblle({
+    apiKey: process.env.TREBLLE_API_KEY,
+    projectId: process.env.TREBLLE_PROJECT_ID,
+  })
+)
+
+// response
 app.use(async (ctx) => {
-  ctx.body = 'Hello World'
+  ctx.body = { message: 'Hello World' }
 })
 
 app.listen(3000)
